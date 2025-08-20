d7511709f341cec15d3581eb4fd868e7fce32300
lorenacampuzano <lorenacampuzano.s@gmail.com>
2025-08-20 16:13:37 -0400

    Lab Scope practice task

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -21,6 +21,7 @@ console.log(globalConst); // Output: "I'm a global constant"
 //console.log(blockLet);
 
 // Function Scope
+/*
 function show(){
     var functionVar = "I'm a block-scoped var";
     let functionLet = "I'm a block-scoped let";
@@ -31,3 +32,27 @@ show();
 console.log(functionVar); // Throws ReferenceError
 console.log(functionLet); // Throws ReferenceError
 console.log(functionConst); // Throws ReferenceError
+*/
+
+//Practice task
+
+{
+// 1. Create one block using {} and declare variables using let, const, and var.
+var nombre = "Alexandra David-Neel"
+let puesto = "BI Developer Jr"
+const $sueldo = 15000
+
+//2. Then, try to reassign these variables within the curly braces {}. Check for any errors that may occur.
+nombre = "Alexandra" // No error
+puesto = "BI Developer Mid" // No error
+//$sueldo = 25000 // TypeError: Assignment to constant variable.
+}
+
+// 3. Next, try to reassign the same variables outside the block {} and check if assignment outside the sope of variables where the variables are assigned can be done or not.
+
+nombre = "Alexandra DL" // No error
+puesto = "BI Developer Sr" // No error
+$sueldo = 40000 // No error
+
+
+
