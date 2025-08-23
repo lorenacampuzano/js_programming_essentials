61d58fd0e9a726ccc7154c830982dac6a64f0e12
lorenacampuzano <lorenacampuzano.s@gmail.com>
2025-08-23 15:55:57 -0400

    Lab JS Function practice task

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -15,3 +15,27 @@ function calculateArea(){
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`; //The backticks and ${} notation allow for the inclusion of JavaScript variables within a string (using template literals)
 }
 
+/*
+Practice task
+
+2. Include the following in the JavaScript file:
+
+    * Create a function which will accept these amount entered by users as a parameter.
+    * Then write the logic to calculate the total amount spent on the grocery purchase.
+    * Call this function in such a way so that after clicking on the button, it shows the total amount for the grocery purchase.
+*/
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function groceryTracker(){
+    
+    grocery1 = parseFloat(document.getElementById('grocery1').value)
+    grocery2 = parseFloat(document.getElementById('grocery2').value)
+    grocery3 = parseFloat(document.getElementById('grocery3').value)
+
+    let total_amount = grocery1 + grocery2 + grocery3
+
+    document.getElementById('result2').innerText = `The total amount is: $${total_amount}`
+}
