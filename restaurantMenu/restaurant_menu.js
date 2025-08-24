31979a847a940eb1617e08ddb28d550a2b459c7c
lorenacampuzano <lorenacampuzano.s@gmail.com>
2025-08-24 12:19:19 -0400

    Adding summary notes

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,3 +1,19 @@
+/*
+Summary
+
+1. HTML structure: Defines an HTML structure for a restaurant menu, categorizing items into Breakfast, Main Course, and Dessert sections using appropriate <h2> headings and <div> elements with unique IDs for content display.
+
+2. JavaScript dynamic content: Utilizes JavaScript to dynamically populate menu items within their respective sections.
+
+    * Breakfast menu: Uses the map method to generate HTML strings for breakfast items and inserts them into the designated <div> element.
+    * Main course menu: Utilizes forEach to construct HTML strings for main course items, updating the related <div> with the generated content.
+    * Dessert menu: Utilizes a for loop to create HTML strings for dessert items and inserts them into the designated <div> element for dessert items.
+
+3. Data handling: Stores menu item data in separate JavaScript arrays (breakfastMenu, mainCourseMenu, dessertMenu) to facilitate dynamic content creation without hardcoding within the HTML structure.
+
+4. Dynamic rendering: Illustrates how JavaScript interacts with HTML, dynamically generating and updating content within the webpage, demonstrating different iteration methods to present menu items under specific categories.
+*/
+
 // Defining variables and map array method
 
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
