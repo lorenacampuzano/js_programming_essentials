3e20d6fbcd1940892ca478c9c4979b562ab74bb0
lorenacampuzano <lorenacampuzano.s@gmail.com>
2025-08-24 15:20:13 -0400

    Lab Array Methods practice task

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,8 +1,9 @@
 // Defining variables and functions
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+     // Practice task: 2. For this, include one more key value pair for employees array of object inside each object as shown in given screenshot (specialization).
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:"JavaScript" },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:"Excel" },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: "SAS" },
     //... More employee records can be added here
   ];
 
@@ -36,3 +37,22 @@ function findEmployeeById(employeeId) {
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
      }
  }
+
+ /*
+Practice task
+
+1. In this practice task, you need to perform a functionality in which information can be fetched on the basis of employee's speciaization as well.
+
+4. Next, create a JavaScript function to display the details of employees who have specialization in JavaScript. You can refer to the findEmployeeById function in the JavaScript code of this lab for guidance.
+
+ */
+
+function findSpecialization(specialization){
+    const foundEmployee = employees.find(employee => employee.specialization === specialization);
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary} - ${foundEmployee.specialization}</p>`;
+    }
+    else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialization';
+    }
+}
